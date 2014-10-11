a72d9bfe7b5f19cfc5c9e94cc318b95e059c8534
nebch13 <noet_09@hotmail.com>
2014-10-11 15:17:43 -0500

    primer commit

---
+++ Multiplos/NumeroMultiplo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplos
+{
+   public class NumeroMultiplo
+    {
+       public string GetMultiploNumero(int num) {
+           var respuesta = "";
+
+
+
+
+           if (num % 3 == 0 && num % 5 == 0) {
+               respuesta = "FizzBuzz";
+           }
+           else if (num % 3 == 0)
+           {
+               respuesta = "Fizz";
+           }
+           else if (num % 5 == 0)
+           {
+               respuesta = "Buzz";
+           }
+           else
+           {
+               respuesta = Convert.ToString(num);
+           }
+              
+           
+           return respuesta;
+       
+       }
+    }
+}
---
+++ MultiplosTest/Test.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Multiplos;
+
+namespace MultiplosTest
+{
+    [TestFixture]
+   public class Test
+    {
+
+        [Test]
+        public void TestNumero1()
+        {
+            //arranque
+            var numero = new NumeroMultiplo();
+
+            var num = numero.GetMultiploNumero(1);
+
+            Assert.AreEqual("1", num);
+        }
+
+        [Test]
+        public void TestNumero2()
+        {
+            //arranque
+            var numero = new NumeroMultiplo();
+
+            var num = numero.GetMultiploNumero(2);
+
+            Assert.AreEqual("2", num);
+        }
+
+        [Test]
+        public void TestNumero3()
+        {
+            //arranque
+            var numero = new NumeroMultiplo();
+
+            var num = numero.GetMultiploNumero(3);
+
+            Assert.AreEqual("Fizz", num);
+        }
+
+        [Test]
+        public void TestNumero4()
+        {
+            //arranque
+            var numero = new NumeroMultiplo();
+
+            var num = numero.GetMultiploNumero(4);
+
+            Assert.AreEqual("4", num);
+        }
+
+        [Test]
+        public void TestNumero5()
+        {
+            //arranque
+            var numero = new NumeroMultiplo();
+
+            var num = numero.GetMultiploNumero(5);
+
+            Assert.AreEqual("Buzz", num);
+        }
+
+        [Test]
+        public void TestNumero15()
+        {
+            //arranque
+            var numero = new NumeroMultiplo();
+
+            var num = numero.GetMultiploNumero(15);
+
+            Assert.AreEqual("FizzBuzz", num);
+        }
+    }
+}
